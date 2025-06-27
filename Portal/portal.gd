6fddfd653ba8955f0052b94d2bacf2344204d151
Matt <matt@le-dahu-studio.ch>
2025-06-28 01:10:37 +0200

    portals

---
+++ Portal/portal.gd
@@ -0,0 +1,9 @@
+extends Area2D
+class_name Portal
+
+@export var connected_portal: Portal
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.time_since_last_teleport > body.teleport_cooldown:
+		body.time_since_last_teleport = 0.0
+		body.global_position = connected_portal.global_position
