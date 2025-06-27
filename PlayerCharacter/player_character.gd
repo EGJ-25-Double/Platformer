cc173a612f6fa6dc26954f46db6d7e3b1759a0bb
pascal-nil <pbenoit@student.42.fr>
2025-06-28 01:21:59 +0200

    player  is done

--- PlayerCharacter/player_character.gd
+++ PlayerCharacter/player_character.gd
@@ -1,13 +1,20 @@
-extends RigidBody2D
+extends CharacterBody2D
 
 @export var speed = 400
+@export var jump_impulse = 400
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("jump") and is_on_floor() :
+		print(is_on_floor())
+		velocity.y = -jump_impulse
 
 func _physics_process(delta: float) -> void:
-	var velocity = Vector2.ZERO
+	if not is_on_floor():
+		velocity.y += gravity * delta
 	if Input.is_action_pressed("move_left"):
-		velocity.x += -1
+		velocity.x += -1  * speed
 	if Input.is_action_pressed("move_right"):
-		velocity.x += 1
-		
-	if velocity.length() > 0:
-		linear_velocity = velocity.normalized() * speed
+		velocity.x += 1 * speed
+	move_and_slide()
