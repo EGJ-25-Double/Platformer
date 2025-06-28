4bade319c86cfa79c3c456f2a14e9f8f228bfa04
m.conti <conti.matthieu@gmail.com>
2025-06-28 18:04:55 +0200

    climb

--- Ladder/ladder.gd
+++ Ladder/ladder.gd
@@ -5,11 +5,17 @@ class_name Ladder
 func _on_body_entered(body: Node2D) -> void:
 	if body is not PlayerCharacter :
 		return
-	body.get_node("Climb").process_mode = Node.PROCESS_MODE_INHERIT
+	var power = body.get_node("Power/Climb")
+	if not power:
+		return
+	power.process_mode = Node.PROCESS_MODE_INHERIT
 
 func _on_body_exited(body: Node2D) -> void:
 	if body is not PlayerCharacter :
 		return
-	body.get_node("Climb").process_mode =  Node.PROCESS_MODE_DISABLED
+	var power = body.get_node("Power/Climb")
+	if not power:
+		return
+	body.get_node("Power/Climb").process_mode =  Node.PROCESS_MODE_DISABLED
 	
 	
--- power/climb.gd
+++ power/climb.gd
@@ -1,12 +1,15 @@
-extends Node
+extends Power
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+func _ready():
+	self.process_mode = Node.PROCESS_MODE_DISABLED
+	self.name = "Climb"
+
 func _physics_process(delta: float) -> void:
-	var direction = 0.0
-	$"..".velocity.y -= gravity * delta
+	player.velocity.y = -gravity * delta
 	if Input.is_action_pressed("move_top") or Input.is_action_pressed("jump"):
-		$"..".velocity.y -= 400
+		player.velocity.y -= 400
 	if Input.is_action_pressed("move_down"):
-		$"..".velocity.y += 400
+		player.velocity.y += 400
 		
