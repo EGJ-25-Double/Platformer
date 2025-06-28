8b0a119e4a123dbd2f531d5cbf551fddd9c96a68
m.conti <conti.matthieu@gmail.com>
2025-06-28 21:34:11 +0200

    masks

--- power/power.gd
+++ power/power.gd
@@ -1,6 +1,12 @@
 class_name Power extends Node
 
 var player: PlayerCharacter
+@export var withOwner := false
+
+
+func _ready():
+	if withOwner:
+		set_player(owner)
 
 func set_player(_player: PlayerCharacter):
 	player = _player
