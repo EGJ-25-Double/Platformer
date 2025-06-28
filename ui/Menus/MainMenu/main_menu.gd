f6984179961ad851edb08349aedcc9df3ad099b4
pascal-nil <pbenoit@student.42.fr>
2025-06-29 00:56:35 +0200

    Menu done

--- ui/Menus/MainMenu/main_menu.gd
+++ ui/Menus/MainMenu/main_menu.gd
@@ -13,10 +13,17 @@ func _input(event):
 func _on_play_btn_pressed() -> void:
 	get_tree().change_scene_to_file("res://map/Map.tscn")
 
+func _on_options_btn_pressed() -> void:
+	$OptionsUI.visible = true
+	$OptionsUI/OptionsBackBtn.grab_focus()
 
 func _on_help_btn_pressed() -> void:
 	$HelpUI.visible = true
 	$HelpUI/HelpBackBtn.grab_focus()
+	
+	
+func _on_help_link_button_pressed() -> void:
+	OS.shell_open("https://google.com/")
 
 
 func _on_credits_btn_pressed() -> void:
@@ -36,3 +43,18 @@ func _on_help_back_btn_pressed() -> void:
 func _on_credits_back_btn_pressed() -> void:
 	$CreditsUI.visible = false
 	$VBoxContainer/PlayBtn.grab_focus()
+
+
+func _on_options_back_btn_pressed() -> void:
+	$OptionsUI.visible = false
+	$VBoxContainer/PlayBtn.grab_focus()
+
+## CHANGE VALUE OF OPTIONS 
+func _on_slider_1_value_changed(value: float) -> void:
+	$OptionsUI/value1.text = str(value)
+
+func _on_slider_2_value_changed(value: float) -> void:
+	$OptionsUI/value2.text = str(value)
+
+func _on_slider_3_value_changed(value: float) -> void:
+	$OptionsUI/value2.text = str(value)
