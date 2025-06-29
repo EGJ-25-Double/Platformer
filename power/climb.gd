3b2f8b5dce9e076ab9ce45f43a482c9e3c287a7a
Aurelie Stephan <contretemps@Aurelies-MacBook-Pro-2.local>
2025-06-29 11:00:39 +0200

    tutututu

--- power/climb.gd
+++ power/climb.gd
@@ -11,7 +11,7 @@ func _physics_process(delta: float) -> void:
 	if not climbing:
 		return
 	player.velocity.y = -gravity * delta
-	if Input.is_action_pressed("move_top"): # or Input.is_action_pressed("jump"):
+	if Input.is_action_pressed("move_top") or Input.is_action_pressed("jump"):
 		player.velocity.y -= 400
 	if Input.is_action_pressed("move_down"):
 		player.velocity.y += 400
