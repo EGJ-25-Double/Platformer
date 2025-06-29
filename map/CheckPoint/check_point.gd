1ce2997fb340b5a8b278f5ee14075ec18407a1b3
m.conti <conti.matthieu@gmail.com>
2025-06-29 13:19:28 +0200

    remove checkpoint

--- map/CheckPoint/check_point.gd
+++ map/CheckPoint/check_point.gd
@@ -4,23 +4,10 @@ class_name CheckPoint extends Node2D
 
 
 func _on_area_2d_body_entered(body):
-	if not body is PlayerCharacter:
-		return
-	activate()
+	pass
 
 func activate():
-	$AudioStreamPlayer2D.play(0.04)
-	for other in get_tree().get_nodes_in_group("checkpoints"):
-		if other == self:
-			continue
-		other.deactivate()
-	GameState.current_checkpoint = self
-	$SpriteActive.visible = true
-	$SpriteDisable.visible = false
-
-	GameState.player.show_key_checkpoint()
+	pass
 
 func deactivate():
-	$SpriteActive.visible = false
-	$SpriteDisable.visible = true
 	pass
