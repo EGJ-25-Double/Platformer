f4f1cafd042ef4e9bb985c63f2e99fe3d1df2590
m.conti <conti.matthieu@gmail.com>
2025-06-29 15:37:40 +0200

    sound

--- area/spawn_on_stay_inside.gd
+++ area/spawn_on_stay_inside.gd
@@ -12,3 +12,13 @@ func _on_animated_sprite_2d_2_animation_finished():
 
 func _on_costume_pickable_tree_exited():
 	queue_free()
+
+
+func _on_area_2d_area_entered(area):
+	(GameState.music as AudioStreamPlayer).volume_db = -80.0
+	pass # Replace with function body.
+
+
+func _on_area_2d_area_exited(area):
+	(GameState.music as AudioStreamPlayer).volume_db = 0
+	pass # Replace with function body.
