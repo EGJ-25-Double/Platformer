a672b064e3053d3a15783a40e73bc6ef28fe5ecc
m.conti <conti.matthieu@gmail.com>
2025-06-29 02:12:25 +0200

    anim change skin

--- PlayerCharacter/power_selection.gd
+++ PlayerCharacter/power_selection.gd
@@ -3,11 +3,14 @@ extends Node
 @onready var player = owner as PlayerCharacter
 var current_skin := 0
 
+var PowerChange = preload("res://power/PowerChange.tscn")
+
 func _input(event):
 	if event.is_action_pressed("change_skin"):
 		get_next_skin()
 
 func set_current_skin():
+	create_anim()
 	for child in get_children():
 		remove_child(child)
 		child.queue_free()
@@ -27,5 +30,13 @@ func set_current_skin():
 		skin.visible = true
 
 func get_next_skin():
-	current_skin = (current_skin + 1) % len(GameState.skins_owned)
+	var new_skin = (current_skin + 1) % len(GameState.skins_owned)
+	if current_skin == new_skin:
+		return
+	current_skin = new_skin
 	set_current_skin()
+
+func create_anim():
+	var node = PowerChange.instantiate() as Node2D
+	node.global_position = player.global_position
+	get_tree().root.add_child(node)
